8bc7b73a65ef3abe0c21ee48c9085db201f07351
MegaBit <MegaBit@MEGABITPC>
2017-03-13 23:14:19 +0100

    Add project files.

---
+++ HookMe/Models/AccountViewModels.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using static HookMe.Models.RegisterViewModel;
+
+namespace HookMe.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Miejscowość")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Płeć")]
+        public int Gender { get; set; }
+
+        [Required]
+        [Display(Name = " ")]
+        [MustBeChecked(ErrorMessage = "Musisz oświadczyć, że jesteś pełnoletni.")]
+        public bool Adult { get; set; }
+
+        [Display(Name = " ")]
+        [MustBeChecked(ErrorMessage = "Zgoda na wysyłanie informacji mailem jest wymagana.")]
+        public bool MailAcceptance { get; set; }
+
+        [StringLength(100, ErrorMessage = "Minimalna długość hasła to {2} znaków.", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+
+    public class SendCodeViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+        public string ReturnUrl { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Provider { get; set; }
+
+        [Required]
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+        public string ReturnUrl { get; set; }
+
+        [Display(Name = "Remember this browser?")]
+        public bool RememberBrowser { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]        
+        [Display(Name = "Miejscowość")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Płeć")]
+        public int Gender { get; set; }
+
+        [Required]        
+        [Display(Name = " ")]        
+        [MustBeChecked(ErrorMessage = "Musisz oświadczyć, że jesteś pełnoletni.")]
+        public bool Adult { get; set; }
+
+        [Required]        
+        [Display(Name = " ")]
+        [MustBeChecked(ErrorMessage = "Zgoda na wysyłanie informacji mailem jest wymagana.")]
+        public bool MailAcceptance { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Minimalna długość hasła to {2} znaków.", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Powtórz hasło")]
+        [Compare("Password", ErrorMessage = "Hasła nie pasują.")]
+        public string ConfirmPassword { get; set; }
+
+        public class MustBeCheckedAttribute : ValidationAttribute
+        {
+            public override bool IsValid(object value)
+            {
+                return value is bool && (bool)value;
+            }
+        }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
